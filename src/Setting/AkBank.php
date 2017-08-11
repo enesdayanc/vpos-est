96e9fc46e2b7b30bcc62917995633b0550202a05
enesdayanc <enesdayanc@gmail.com>
2017-08-11 17:21:01 +0300

    add AkBank live settings

---
+++ src/Setting/AkBank.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: enesdayanc
+ * Date: 11/08/2017
+ * Time: 17:20
+ */
+
+namespace Enesdayanc\VPosEst\Setting;
+
+
+class AkBank extends Setting
+{
+    private $host = "www.sanalakpos.com";
+
+    public function getThreeDPostUrl()
+    {
+        return "https://" . $this->host . "/fim/est3Dgate";
+    }
+
+    public function getAuthorizeUrl()
+    {
+        return "https://" . $this->host . "/servlet/cc5ApiServer";
+    }
+
+    public function getCaptureUrl()
+    {
+        return "https://" . $this->host . "/servlet/cc5ApiServer";
+    }
+
+    public function getPurchaseUrl()
+    {
+        return "https://" . $this->host . "/servlet/cc5ApiServer";
+    }
+
+    public function getRefundUrl()
+    {
+        return "https://" . $this->host . "/servlet/cc5ApiServer";
+    }
+
+    public function getVoidUrl()
+    {
+        return "https://" . $this->host . "/servlet/cc5ApiServer";
+    }
+}
