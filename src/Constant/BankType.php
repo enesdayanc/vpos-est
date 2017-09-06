181a088c500e319c68d8b6c54834fba7b7cdeabb
enesdayanc <enesdayanc@gmail.com>
2017-09-06 09:41:57 +0300

    add bank type constant

---
+++ src/Constant/BankType.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: enesdayanc
+ * Date: 06/09/2017
+ * Time: 09:39
+ */
+
+namespace PaymentGateway\VPosEst\Constant;
+
+
+class BankType
+{
+    const AKBANK = 'AKBANK';
+    const FINANSBANK = 'FINANSBANK';
+    const TURK_EKONOMI_BANKASI = 'TURK_EKONOMI_BANKASI';
+    const TURKIYE_IS_BANKASI = 'TURKIYE_IS_BANKASI';
+}
